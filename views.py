f2af09b327c793f28a4b4c8bffb9149cbd295b00
shuwei05 <zzzzzzyoop@gmail.com>
2025-08-23 15:57:55 +0800

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-views = Blueprint('views',__name__)
-
-@views.route('/aboutus')
-def home():
-    return render_template('aboutus.html', text='Home Page')
